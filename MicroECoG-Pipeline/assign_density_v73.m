57b0b132abbed2cd09a810ad2800583e409c6373
Jaejin Lee <jaejinlee137@gmail.com>
2024-05-02 12:45:34 -0500

    initial commit

--- MicroECoG-Pipeline/assign_density_v73.m
+++
@@ -1,4 +0,0 @@
-decAccDensityCh = cell(3,1);
-decAccDensityCh{1} = input('Enter low density channels: ');
-decAccDensityCh{2} = input('Enter mid density channels: ');
-decAccDensityCh{3} = top20(:,1)';
